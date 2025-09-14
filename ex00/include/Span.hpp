7e50f94d1b3fdc77b583ee143d34635331f8f547
Clement Colin <clmnt.co@gmail.com>
2025-09-15 08:40:43 +0900

    Quick commit

---
+++ ex00/include/Span.hpp
@@ -0,0 +1,13 @@
+class Span
+{
+private:
+	
+public:
+	Span();
+	Span(const Span &other);
+	Span &operator=(const Span &other);
+	~Span();
+
+	
+
+};
---
+++ ex00/src/Span.cpp
@@ -0,0 +1,19 @@
+#include "Span.hpp"
+
+Span::Span() {}
+
+Span::Span(const Span &other)
+{
+	// copy members here
+}
+
+Span &Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
+		// copy members here
+	}
+	return (*this);
+}
+
+Span::~Span() {}
