3941e86920b28e3b8d980b51d7acc92aa2f1fd9d
ccolin <clmnt.co@gmail.com>
2025-12-08 17:33:34 +0900

    Quick commit

--- ex01/include/Span.hpp
+++ ex01/include/Span.hpp
@@ -1,13 +1,22 @@
+#pragma once
+
+#include <vector>
+
 class Span
 {
-private:
-	
 public:
-	Span();
+	Span(unsigned int n);
 	Span(const Span &other);
 	Span &operator=(const Span &other);
 	~Span();
 
-	
+	void addNumber(int number);
+	template <typename InputIt>
+	void addNumbers(InputIt begin, InputIt end);
+	int shortestSpan();
+	int longestSpan();
 
+private:
+	unsigned int n;
+	std::vector<int> v;
 };
--- ex01/src/Span.cpp
+++ ex01/src/Span.cpp
@@ -1,19 +1,71 @@
 #include "Span.hpp"
+#include <stdexcept>
+#include <algorithm>
 
-Span::Span() {}
+Span::Span(unsigned int n) : n(n) {}
 
-Span::Span(const Span &other)
-{
-	// copy members here
-}
+Span::Span(const Span &other) : v(other.v), n(other.n) {}
 
 Span &Span::operator=(const Span &other)
 {
 	if (this != &other)
 	{
-		// copy members here
+		n = other.n;
+		v = other.v;
 	}
 	return (*this);
 }
 
 Span::~Span() {}
+
+void Span::addNumber(int number)
+{
+	if (v.size() + 1 > n)
+		throw std::runtime_error("Span is full");
+	v.push_back(number);
+}
+
+template <typename InputIt>
+void Span::addNumbers(InputIt begin, InputIt end)
+{
+    size_t rangeSize = std::distance(begin, end);
+    if (v.size() + rangeSize > n)
+        throw std::runtime_error("Adding range would exceed Span capacity");
+
+    while (begin != end)
+    {
+        v.push_back(*begin);
+        ++begin;
+    }
+}
+
+int Span::shortestSpan()
+{
+	unsigned int shortestSpan = 0;
+
+	if (v.size() == 0 || v.size() == 1)
+		throw std::runtime_error("No span can be found");
+
+	std::sort(v.begin(), v.end());
+	for (size_t i = 0; i < v.size() - 1; i++)
+	{
+		if (i == 0)
+		{
+			shortestSpan = v[i + 1] - v[i];
+				continue;
+		}
+		int diff = v[i + 1] - v[i];
+		if (diff < shortestSpan)
+			shortestSpan = diff;
+	}
+	return shortestSpan;
+}
+
+int Span::longestSpan()
+{
+	if (v.size() == 0 || v.size() == 1)
+		throw std::runtime_error("No span can be found");
+	std::sort(v.begin(), v.end());
+	return v[v.size() - 1] - v[0];
+}
+
